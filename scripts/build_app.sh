36aa0901f392ba203a427d0a215b76047635c72e
valabji-wbhr <117309695+valabji-wbhr@users.noreply.github.com>
2026-05-25 17:30:49 +0300

    Bump to v0.1.16 and add release notes

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -14,7 +14,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_DIR="$(dirname "$REPO_DIR")"
 PLATFORM="$(uname)"
 APP_NAME="ValScanner"
-VERSION="0.1.15"
+VERSION="0.1.16"
 MAKE_DMG=0
 SIGN_ID=""
 
